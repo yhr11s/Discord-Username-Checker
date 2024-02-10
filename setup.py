e9c9d8173628e11eac6e9697e6bc40e3b53fc173
yhr11s <144565884+yhr11s@users.noreply.github.com>
2024-02-11 00:30:47 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name= 'Discord Username Checker',
+    version= '0.1',
+    author= 'Hassan',
+    description= 'Discord Username Checker By Hassan',
+    packages=setuptools.find_packages(),
+    classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    "License :: OSI Approved :: MIT License"
+    ]
+)
